587eb48783621cd690042c144f18c477f8759c15
timof121 <52547763+timof121@users.noreply.github.com>
2022-04-01 18:08:33 +0200

    added a very strict anti pearl glitch (commented since too unstable)

--- PearlFix/src/main/java/me/timof121/pearlfix/ListenerPearl.java
+++ PearlFix/src/main/java/me/timof121/pearlfix/ListenerPearl.java
@@ -1,14 +1,16 @@
 package me.timof121.pearlfix;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 
 public class ListenerPearl implements Listener {
     @EventHandler
     public void onPearl(org.bukkit.event.player.PlayerTeleportEvent e) {
         if (e.getCause() == org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.ENDER_PEARL) {
-            e.getPlayer().teleport(e.getPlayer().getLocation().add(e.getPlayer().getLocation().getDirection().multiply(-0.5D)));
             Location location = e.getTo();
             location.setX(location.getBlockX() + 0.5D);
             location.setY(location.getBlockY());
@@ -16,4 +18,13 @@ public class ListenerPearl implements Listener {
             e.setTo(location);
         }
     }
-        }
+    //@EventHandler
+    //public void onClick(org.bukkit.event.player.PlayerInteractEvent e) {
+        //if (e.getPlayer().getItemInHand().getType() == org.bukkit.Material.ENDER_PEARL) {
+            //if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                //e.setCancelled(true);
+            //}
+        //}
+    //}
+
+}
