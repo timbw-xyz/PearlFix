8f2ccd9fa79af8f6344c1d9da8772faee5709304
timof121 <52547763+timof121@users.noreply.github.com>
2022-04-04 14:29:19 +0200

    Finally added an anti fence pearl glitcher!
    suffering

--- PearlFix/src/main/java/me/timof121/pearlfix/ListenerPearl.java
+++ PearlFix/src/main/java/me/timof121/pearlfix/ListenerPearl.java
@@ -1,13 +1,14 @@
 package me.timof121.pearlfix;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 
 public class ListenerPearl implements Listener {
+
     @EventHandler
     public void onPearl(org.bukkit.event.player.PlayerTeleportEvent e) {
         if (e.getCause() == org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.ENDER_PEARL) {
@@ -17,7 +18,15 @@ public class ListenerPearl implements Listener {
             location.setZ(location.getBlockZ() + 0.5D);
             e.setTo(location);
         }
+        Location to = e.getTo();
+        if (to.getBlock().getType() == Material.FENCE_GATE) {
+            Player player = e.getPlayer();
+            player.sendMessage(ChatColor.RED + "(Server) Please don't pearl glitch!");
+            e.setCancelled(true);
+            e.getPlayer().getInventory().addItem(new org.bukkit.inventory.ItemStack(Material.ENDER_PEARL));
+        }
     }
+
     //@EventHandler
     //public void onClick(org.bukkit.event.player.PlayerInteractEvent e) {
         //if (e.getPlayer().getItemInHand().getType() == org.bukkit.Material.ENDER_PEARL) {
