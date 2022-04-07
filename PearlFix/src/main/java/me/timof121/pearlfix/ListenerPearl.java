1dac142c423bcddf89ad51dcd6984a5819433833
timof121 <52547763+timof121@users.noreply.github.com>
2022-04-07 22:03:47 +0200

    Push some stuff & fixes

--- PearlFix/src/main/java/me/timof121/pearlfix/ListenerPearl.java
+++ PearlFix/src/main/java/me/timof121/pearlfix/ListenerPearl.java
@@ -5,21 +5,22 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 public class ListenerPearl implements Listener {
 
-    @EventHandler
+    @EventHandler()
     public void onPearl(org.bukkit.event.player.PlayerTeleportEvent e) {
-        if (e.getCause() == org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.ENDER_PEARL && e.getPlayer().getTicksLived() > 20) {
+        Location to = e.getTo();
+        if (e.getCause() == org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.ENDER_PEARL && to.getBlock().getType() != Material.FENCE_GATE) {
             Location location = e.getTo();
-            location.setX(location.getBlockX() + 0.25D);
+            location.setX(location.getBlockX() + 0.5D);
             location.setY(location.getBlockY());
-            location.setZ(location.getBlockZ() + 0.25D);
+            location.setZ(location.getBlockZ() + 0.5D);
             e.setTo(location);
         }
-        Location to = e.getTo();
-        if (to.getBlock().getType() == Material.FENCE_GATE) {
+        if (to.getBlock().getType() == Material.FENCE_GATE && e.getCause() == org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.ENDER_PEARL) {
             Player player = e.getPlayer();
             player.sendMessage(ChatColor.RED + "[!] Pearl glitching detected");
             e.setCancelled(true);
