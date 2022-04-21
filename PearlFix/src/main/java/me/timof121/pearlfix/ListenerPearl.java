f734b046c23915ff6b84d0ed00ba152ff93583d1
timof121 <52547763+timof121@users.noreply.github.com>
2022-04-21 22:47:22 +0200

    Added anti endermite & fixes

--- PearlFix/src/main/java/me/timof121/pearlfix/ListenerPearl.java
+++ PearlFix/src/main/java/me/timof121/pearlfix/ListenerPearl.java
@@ -3,28 +3,37 @@ package me.timof121.pearlfix;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 
 public class ListenerPearl implements Listener {
 
     @EventHandler()
     public void onPearl(org.bukkit.event.player.PlayerTeleportEvent e) {
-        Location to = e.getTo();
-        if (e.getCause() == org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.ENDER_PEARL && to.getBlock().getType() != Material.FENCE_GATE) {
+        if (e.getCause() == org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.ENDER_PEARL) {
             Location location = e.getTo();
             location.setX(location.getBlockX() + 0.5D);
             location.setY(location.getBlockY());
             location.setZ(location.getBlockZ() + 0.5D);
             e.setTo(location);
         }
-        if (to.getBlock().getType() == Material.FENCE_GATE && e.getCause() == org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.ENDER_PEARL) {
+        Location to = e.getTo();
+        if (to.getBlock().getType() == Material.FENCE_GATE) {
             Player player = e.getPlayer();
-            player.sendMessage(ChatColor.RED + "[!] Pearl glitching detected");
+            player.sendMessage(ChatColor.RED + "[!] Pearl glitching detected!");
             e.setCancelled(true);
             e.getPlayer().getInventory().addItem(new org.bukkit.inventory.ItemStack(Material.ENDER_PEARL));
+        }
+    }
+
+    @EventHandler
+    final void onCreatureSpawnEvent(CreatureSpawnEvent e) {
+        if (e.getEntityType().equals((Object) EntityType.ENDERMITE)) {
+            e.setCancelled(true);
         }
     }
 
