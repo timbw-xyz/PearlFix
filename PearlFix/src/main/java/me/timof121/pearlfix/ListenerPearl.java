8e0a31b468b91579c80d7932e0c43be5fb0341a1
timof121 <52547763+timof121@users.noreply.github.com>
2022-05-25 20:33:55 +0200

    Added a tp cause check

--- PearlFix/src/main/java/me/timof121/pearlfix/ListenerPearl.java
+++ PearlFix/src/main/java/me/timof121/pearlfix/ListenerPearl.java
@@ -1,18 +1,21 @@
 package me.timof121.pearlfix;
 
+//import me.daaz.vapor.VaporAPI;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 public class ListenerPearl implements Listener {
 
-    @EventHandler()
+    //public static VaporAPI vaporAPI;
+
+    @EventHandler
     public void onPearl(org.bukkit.event.player.PlayerTeleportEvent e) {
         if (e.getCause() == org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.ENDER_PEARL) {
             Location location = e.getTo();
@@ -22,11 +25,14 @@ public class ListenerPearl implements Listener {
             e.setTo(location);
         }
         Location to = e.getTo();
-        if (to.getBlock().getType() == Material.FENCE_GATE) {
+        if (to.getBlock().getType() == Material.FENCE_GATE && e.getCause() == org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.ENDER_PEARL) {
             Player player = e.getPlayer();
             player.sendMessage(ChatColor.RED + "[!] Pearl glitching detected!");
             e.setCancelled(true);
             e.getPlayer().getInventory().addItem(new org.bukkit.inventory.ItemStack(Material.ENDER_PEARL));
+            /*if (vaporAPI.getTimerManager().getEnderpearlHandler().hasCooldown(player)){
+                vaporAPI.getTimerManager().getEnderpearlHandler().clearCooldown(player);
+            }*/
         }
     }
 
