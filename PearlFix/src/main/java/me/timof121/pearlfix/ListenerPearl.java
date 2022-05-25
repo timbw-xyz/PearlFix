0b0de1cbdea9eeeb76f71df2bd1789f8d73e870d
timof121 <52547763+timof121@users.noreply.github.com>
2022-05-25 20:22:25 +0200

    added vapor pearl api

--- PearlFix/src/main/java/me/timof121/pearlfix/ListenerPearl.java
+++ PearlFix/src/main/java/me/timof121/pearlfix/ListenerPearl.java
@@ -1,49 +1,18 @@
 package me.timof121.pearlfix;
+import me.daaz.vapor.VaporAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.CreatureSpawnEvent;
+public class Main extends JavaPlugin {
 
-public class ListenerPearl implements Listener {
-
-    @EventHandler()
-    public void onPearl(org.bukkit.event.player.PlayerTeleportEvent e) {
-        if (e.getCause() == org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.ENDER_PEARL) {
-            Location location = e.getTo();
-            location.setX(location.getBlockX() + 0.5D);
-            location.setY(location.getBlockY());
-            location.setZ(location.getBlockZ() + 0.5D);
-            e.setTo(location);
-        }
-        Location to = e.getTo();
-        if (to.getBlock().getType() == Material.FENCE_GATE) {
-            Player player = e.getPlayer();
-            player.sendMessage(ChatColor.RED + "[!] Pearl glitching detected!");
-            e.setCancelled(true);
-            e.getPlayer().getInventory().addItem(new org.bukkit.inventory.ItemStack(Material.ENDER_PEARL));
-        }
+    public void onEnable() {
+        Bukkit.getLogger().info("PearlFix enabled!");
+        getServer().getPluginManager().registerEvents(new ListenerPearl(), this);
+        ListenerPearl.vaporAPI = new VaporAPI();
     }
 
-    @EventHandler
-    final void onCreatureSpawnEvent(CreatureSpawnEvent e) {
-        if (e.getEntityType().equals((Object) EntityType.ENDERMITE)) {
-            e.setCancelled(true);
-        }
+    public void onDisable() {
+        Bukkit.getLogger().info("PearlFix disabled!");
     }
-
-    //@EventHandler
-    //public void onClick(org.bukkit.event.player.PlayerInteractEvent e) {
-        //if (e.getPlayer().getItemInHand().getType() == org.bukkit.Material.ENDER_PEARL) {
-            //if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-                //e.setCancelled(true);
-            //}
-        //}
-    //}
 
 }
